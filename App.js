1e5e8765b707041ca00e13a4fb85c18ec6d5d9a2
luciano1997 <lucianogarridosepulveda@gmail.com>
2025-09-17 14:45:08 -0300

    mejoras

--- App.js
+++ App.js
@@ -1,19 +1,51 @@
 import TabsNavigator from './src/navigation/tabs/TabsNavigator';
-import { NavigationContainer } from "@react-navigation/native";
 import Header from './src/components/Header';
 import { SafeAreaView } from 'react-native';
 import { Provider } from 'react-redux';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import {store} from './src/store/index'
+import * as SplashScreen from 'expo-splash-screen';
+import { store } from './src/store/index';
 import MainNavigator from './src/navigation/MainNavigator';
+import React, { useEffect, useCallback, useState } from 'react';
+import { loadFonts } from './fonts';
+
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    // Simula carga de recursos (fonts, datos, etc.)
+    const prepare = async () => {
+      try {
+        // Aquí puedes cargar fonts, datos, etc.
+        await loadFonts();
+        // Simula una carga de 2 segundos para ver el splash
+        await new Promise(res => setTimeout(res, 2000));
+        // await fetchInitialData();
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    };
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
     <Provider store={store}>
-      <SafeAreaView style={{ flex: 1 }}>
+      <SafeAreaView style={{ flex: 1 }} onLayout={onLayoutRootView}>
         <MainNavigator>
-          {/* <ShopStackNavigation /> */}
-          {/* <CartStackNavigation /> */}
-          <Header />
+          
           <TabsNavigator />
         </MainNavigator>
       </SafeAreaView>
--- src/global/colors.js
+++ src/global/colors.js
@@ -1,13 +1,15 @@
 export const colors = {
-    "darkGray":"#3F3F3F",
-    "mediumGray":"#C6C6C6",
-    "lightGray":"#EBEBEB",
-    "white":"#FFF",
-    "black":"#000",
-    "cobaltBlue":"#0047AB",
-    "neonGreen":"#39FF14",
-    "purple":"#6A1B9A",
-    "yellow":"#FFEB3B",
-    "brightOrange":"#FF5722",
-    "red":"#cd3327"
-}
+    primary: "#F3C16B", // Amarillo
+    secondary: "#E89B7A", // Naranja
+    white: "#FFFFFF", // Blanco
+    accent: "#A67C52", // Marrón claro
+    text: "#222222", // Texto principal
+    textLight: "#FFFFFF", // Texto sobre fondo oscuro
+    lightGray: "#F5F5F5", // Gris claro
+    grey: "#E0E0E0", // Gris medio
+    darkGray: "#A9A9A9", // Gris oscuro
+    error: "#FF4C4C", // Rojo para errores
+    success: "#4CAF50", // Verde para éxito
+    paper: "#FAFAFA", // Fondo de papel
+
+};
